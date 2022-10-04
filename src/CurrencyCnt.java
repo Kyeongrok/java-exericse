0b83008849a065c09027d018ff4f75a92141e8ad
Kyeongrok <oceanfog1@gmail.com>
2022-10-04 14:25:19 +0900

    몫과 나머지 구하기 1. 50000 ~ 500까지 나눈 몫과 나머지 출력 2. 생각 떠올리기

---
+++ src/CurrencyCnt.java
@@ -0,0 +1,29 @@
+public class CurrencyCnt {
+
+    public static void main(String[] args) {
+        // 현금으로 돈을 받고 25000원을 거슬러 주어야 합니다.
+        // 5만원짜리는 몇장 만원짜리는 몇장 5천원짜리는 몇장을 거슬러 주어야 할까요?
+
+        int curr50000 = 50000;
+        int curr10000 = 10000;
+        int curr5000 = 5000;
+        int curr1000 = 1000;
+        int curr500 = 500;
+
+        // 50,000 0장
+        // 10,000
+        // 5,000
+        // 1,000
+        // 500
+
+        // raw 익히지 않은
+        int refund = 25000; // 25000이라면 만원짜리 2장 5000원짜리 1장
+        System.out.printf("5만원권 %d장 나머지:%d\n", refund / curr50000, refund % curr50000); // 25000을 5만원으로 거슬러주면?
+        // 25,000원을 5만원으로 나누었을 때 몫, 나머지
+        // 25,000원을 각 화폐 단위로 나누었을 때 몫과 나머지를 출력하는 코드를 작성해주세요.
+        // 각 화폐 단위는 위와 같이 각각 변수로 선언 해주세요.
+        System.out.printf("1만원권 몫:%d장 나머지:%d\n", refund / curr10000, refund % curr10000); // 25000을 5만원으로 거슬러주면?
+        System.out.printf("5000원권 몫:%d장 나머지:%d\n", refund / curr5000, refund % curr5000); // 25000을 5만원으로 거슬러주면?
+
+    }
+}
