9422166d852066fe875f9f61a7abb68e6771dc3c
Kyeongrok <oceanfog1@gmail.com>
2022-10-04 13:18:16 +0900

    Array10개 1. array10칸 짜리 만들고 2. 1~10까지 채우기

---
+++ src/ArrayTest.java
@@ -0,0 +1,8 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.println("hello"); // alt + shift + f10
+        // 10칸의 int array를 만들고
+        // 1부터 10까지 채워보세요
+        // 그리고 console에 출력 해보세요.
+    }
+}
